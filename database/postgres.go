7edf0bfb82e3f5a57daab001ddbc03fad676c60b
Vincent Wijaya <vincent.wijaya@mekari.com>
2023-04-05 15:17:37 +0700

    use config from env when exist

--- database/postgres.go
+++ database/postgres.go
@@ -9,29 +9,46 @@ import (
 	"gorm.io/gorm"
 )
 
-const (
+type Database struct {
+	Host      string
+	Username  string
+	Password  string
+	Port      int
+	Name      string
+	DebugMode string
+}
+
+var (
 	DB_HOST     = "localhost"
 	DB_USER     = "postgres"
 	DB_PASSWORD = "test123456"
 	DB_PORT     = 5432
 	DB_NAME     = "test"
-	DEBUG_MODE  = true // true/false
+	DEBUG_MODE  = false // true/false
+	db          *gorm.DB
+	err         error
 )
 
-var (
-	db  *gorm.DB
-	err error
-)
-
-func Postgres() *gorm.DB {
+func Postgres(conf *Database) *gorm.DB {
 	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
 		DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+
+	if conf.Host != "" {
+		config = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
+			conf.Host, conf.Username, conf.Password, conf.Name, conf.Port)
+	}
 
 	db, err = gorm.Open(postgres.Open(config), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 
-	db.Debug().AutoMigrate(models.Product{}, models.User{})
+	if conf.DebugMode == "true" || DEBUG_MODE {
+		fmt.Println(conf.DebugMode, DEBUG_MODE)
+		db.Debug().AutoMigrate(models.Product{}, models.User{})
+		return db
+	}
+
+	db.AutoMigrate(models.User{}, models.Product{})
 	return db
 }
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"belajar-gin-gorm/controllers"
 	"belajar-gin-gorm/database"
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,7 +15,17 @@ const PORT = ":3000"
 func main() {
 	fmt.Println("Starting server.......")
 
-	db := database.Postgres()
+	dbPort, _ := strconv.Atoi(os.Getenv("DB_USER"))
+	dbConf := database.Database{
+		Host:      os.Getenv("DB_Host"),
+		Username:  os.Getenv("DB_USER"),
+		Password:  os.Getenv("DB_Password"),
+		Port:      dbPort,
+		Name:      os.Getenv("DB_NAME"),
+		DebugMode: os.Getenv("DEBUG_MODE"),
+	}
+
+	db := database.Postgres(&dbConf)
 	controllers := controllers.New(db)
 
 	r := gin.Default()
